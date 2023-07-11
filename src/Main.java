682ef71553be69b6f662812f51dafe27a2db55b1
Valerii <lord_liret@mail.ru>
2023-07-11 07:28:16 +0500

    Cделано ДЗ. внесены правки:printf заменен на println

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         // Press Alt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Привет, мир!");
+        System.out.println("Привет, мир!");
     }
 }
